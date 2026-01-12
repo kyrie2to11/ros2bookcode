421af16426c77904d6860cca255ac562e82e23be
jarvis65536 <jarvis65536@gmail.com>
2026-01-12 15:21:42 +0800

    feat: 更新 patrol_client 输出设置为 'both'，并添加 VSCode 配置文件

--- chapt4/chapt4_ws/src/demo_cpp_service/launch/demo.launch.py
+++ chapt4/chapt4_ws/src/demo_cpp_service/launch/demo.launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
     action_node_patrol_client = launch_ros.actions.Node(
         package='demo_cpp_service',
         executable="patrol_client",
-        output='log',
+        output='both',
     )
     action_node_turtlesim_node = launch_ros.actions.Node(
         package='turtlesim',
